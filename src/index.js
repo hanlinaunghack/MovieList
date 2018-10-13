d26fd33de3fe0a7245076af69a9e497bf3736955
Han Lin Aung <hanlinaunghack@gmail.com>
2018-10-12 19:48:12 -0700

    did some functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app.js";
+import store from "../store/store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<div>Movie List</div>, document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={store}>
+    <div className="center">
+      <h1>Movie List</h1>
+      <div>
+        <App />
+      </div>
+    </div>
+  </Provider>,
+  document.getElementById("app")
+);
 
 module.hot.accept();
